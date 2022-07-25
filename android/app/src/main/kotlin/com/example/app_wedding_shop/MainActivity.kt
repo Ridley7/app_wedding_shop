f265560ff4f438b7a686a7070dc18ad5fb0771fa
Ridley <gonzalezfranks@hotmail.com>
2022-07-25 09:50:28 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/app_wedding_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_wedding_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryColor = Color(0xFF5C6267);
+const kSizingButtonColor = Color(0xFFF6E2F2);
+const kAccentColor = Color(0xFFF28CD6);
